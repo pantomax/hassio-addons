c2ed5305fcca9b204da7d859483584f22a520485
pantomax <32955378+pantomax@users.noreply.github.com>
2018-02-10 20:07:31 +0100

    a

--- hc-sr04/run.sh
+++ hc-sr04/run.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -e
  
-requirements=$(cat /data/options.json | jq -r 'if .requirements then .requirements | join(" ") else "" end')
 code=$(cat /data/options.json | jq -r '.code')
-clean=$(cat /data/options.json | jq -r '.clean //empty')
 py2=$(cat /data/options.json | jq -r '.python2 // empty')
 
 PYTHON=$(which python3)
