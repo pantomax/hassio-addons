747c2bb44bed7a0eb4d7424d33141725b9d775cd
pantomax <32955378+pantomax@users.noreply.github.com>
2018-02-10 20:20:08 +0100

    a

--- hc-sr04/run.sh
+++ hc-sr04/run.sh
@@ -2,14 +2,23 @@
 set -e
  
 code=$(cat /data/options.json | jq -r '.code')
-py2=$(cat /data/options.json | jq -r '.python2 // empty')
-
-PYTHON=$(which python3)
 
 if [ "${py2}" == "true" ];
 then
     PYTHON=$(which python2)
 fi
 
+if [ "$clean" == "true" ];
+then
+rm -rf /data/venv/
 fi
+
+if [ ! -f "/data/venv/bin/activate" ];
+then
+    mkdir -p /data/venv/
+    cd /data/venv
+    virtualenv -p ${PYTHON} .
+    . bin/activate
+fi
+
 python ${code} 
